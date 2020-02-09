c621678595e78d151a65fb3c9c3ba841630d733f
dean <>
2020-02-08 23:44:11 -0800

    revert to config file instead of automatic

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function AwairLocal(log, config) {
 	this.log = log;
 	this.logging = config["logging"] || false;
 	this.ip = config["ip"];
-	this.manufacturer = config["manufacturer"] || "Awair";
 	this.carbonDioxideThreshold = Number(config["carbonDioxideThreshold"]) || 0; // ppm, 0 = OFF
 	this.carbonDioxideThresholdOff = Number(config["carbonDioxideThresholdOff"]) || Number(config["carbonDioxideThreshold"]); // ppm, same as carbonDioxideThreshold by default, should be less than or equal to carbonDioxideThreshold
 	this.vocMW = Number(config["voc_mixture_mw"]) || 72.66578273019740; // Molecular Weight (g/mol) of a reference VOC gas or mixture
@@ -23,10 +22,10 @@ function AwairLocal(log, config) {
 	this.limit = Number(config["limit"]) || 12; // consecutive 10 second samples averaged (default: 12 x 10 = 120 seconds)
 	this.url = config["url"] || "http://" + this.ip + "/air-data/latest";
 	this.configUrl = config["url"] || "http://" + this.ip + "/settings/config/data";
-	//this.devType = config["devType"] || "awair-omni";
-	//this.model = "unknown";
-	//this.serial = "unknown";
-	//this.version = "unknown";
+	this.manufacturer = config["manufacturer"] || "Awair";
+	this.model = config["model"] || model;
+	this.serial = config["serial"] || "unknown serial";
+	this.version = config["version"] || "unknown version";
 }
 
 AwairLocal.prototype = {
@@ -467,37 +466,6 @@ AwairLocal.prototype = {
 	
 	getServices: function() {
 		var services = [];
-		
-		var that = this;
-		
-		var configOptions = {
-			method: "GET",
-			uri: that.configUrl,
-			json: true
-		};
-		
-		if(that.logging){that.log("[" + that.ip + "] url: " + that.configUrl)};
-		
-		return request(configOptions)
-			.then(function(response) {
-				var configData = response;
-				
-				if(that.logging){that.log("[" + configData.wifi_mac + "] " + that.configUrl + ": " + JSON.stringify(configData))};
-				
-				var devUuid = configData.device_uuid;
-				
-				that.model = devUuid;
-				that.devType = that.model;
-				that.serial = configData.wifi_mac;
-				that.version = configData.fw_version;
-			})
-			.catch(function(err) {
-				if(that.logging){that.log("[" + that.ip + "] " + err)};
-				
-				that.model = "unknown";
-				that.serial = "unknown";
-				that.version = "unknown";
-			});
 		
 		var informationService = new Service.AccessoryInformation();
 		informationService
